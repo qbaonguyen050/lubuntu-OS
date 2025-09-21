29ddda8eded5d7bf3a7f81fef3b0ab015f14add1
schoolprotestanism <qbaonguyen050@gmail.com>
2025-09-21 02:01:56 +0000

    Fix: Create robust startup script

--- .devcontainer/start-desktop.sh
+++ .devcontainer/start-desktop.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Start a virtual screen in the background
+# Clean up any leftover X server lock files
+rm -f /tmp/.X0-lock
+
+# Start a virtual screen in the background as root
 Xvfb :0 -screen 0 1280x800x24 &
 sleep 2
 
-# Set the DISPLAY environment variable
+# Set the DISPLAY environment variable for all subsequent commands
 export DISPLAY=:0
 
-# Start the Lubuntu desktop environment in the background
-startlxqt &
-
-# Start the VNC server in the background
-x11vnc -display :0 -forever -passwd vscode -shared &
-
-# Start the noVNC web client bridge in the background
-websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
+# Use `su` to run the following commands as the 'vscode' user
+su - vscode -c "startlxqt &"
+su - vscode -c "x11vnc -display :0 -forever -passwd vscode -shared &"
+su - vscode -c "websockify --web=/usr/share/novnc/ 6080 localhost:5900 &"
 
 echo "Lubuntu desktop services have been started."
-
-# Keep this script running forever
-sleep infinity
